9451f7d31ef674c40e2333588a635e2425541d74
eiiti <eiiti@mail.ru>
2022-11-06 02:02:29 +0300

    Practice2 functions

---
+++ Quiz2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int length = num.Length;
+
+if(length<3) {
+    Console.WriteLine("третьей цифры нет");
+} else {
+    Console.WriteLine(num[2]);
+}
---
+++ Quiz3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>7 || number1<1)
+{
+    Console.WriteLine("Ошибка");
+}
+
+if(number1 == 1 || number1 == 2||number1 == 3||number1 ==4||number1 ==5)
+{
+    Console.WriteLine("Будний день");
+}
+
+if(number1 == 6 || number1 ==7)
+{
+    Console.WriteLine("Выходной");
+}
